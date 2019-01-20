278b886b047a6ed0a65fe9d2451d25308caeba9d
Alessandro Gentilini <alessandro.gentilini@gmail.com>
2019-01-20 10:01:13 +0100

    Update interp_WSK.m

--- WSK/interp_WSK.m
+++ WSK/interp_WSK.m
@@ -2,7 +2,7 @@ function y = interp_WSK(samples,T_s,t)
   y = zeros(1,length(t));
   for k = 1:1:length(samples)
     % https://en.wikipedia.org/wiki/Whittaker%E2%80%93Shannon_interpolation_formula
-    % use normalized sinc, i.e. sin(pi*x)/(pi*x)
+    % the above formula uses so called "normalized sinc", i.e. sin(pi*x)/(pi*x)
     y+=samples(k)*sin(pi*(t-k*T_s)/T_s)./(pi*(t-k*T_s)/T_s);
   endfor
 endfunction
