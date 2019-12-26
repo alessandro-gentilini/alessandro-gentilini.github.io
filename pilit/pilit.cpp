724452abea85baabcdbc2b8d6eb2845fa0333ff2
Alessandro Gentilini <alessandro.gentilini@gmail.com>
2019-12-26 10:27:18 +0100

    Add search for the whole text

--- pilit/pilit.cpp
+++ pilit/pilit.cpp
@@ -1,23 +1,31 @@
 #include <iostream>
-#include <map>
+#include <utility>
+#include <type_traits>
 
-int T[] = {3,1,4};
-int P[] = {3,1,4,1,5};
+int T[] = {3, 1, 4, 3, 1};
+int P[] = {3, 1, 4, 1, 5};
 
-std::pair<int,int> f(int t, int Tsz, int p, int Psz)
+std::pair<int, int> f(int t, int Tsz, int p, int Psz)
 {
-    if(T[t]==P[p] && t+1<=Tsz && p+1<=Psz){
-        f(t+1,Tsz,p+1,Psz);
-    } else {
-        return std::make_pair(t,p);
+    if (T[t] == P[p] && t + 1 <= Tsz && p + 1 <= Psz)
+    {
+        f(t + 1, Tsz, p + 1, Psz);
+    }
+    else
+    {
+        return std::make_pair(t, p);
     }
 }
 
 int main(int argc, char const *argv[])
 {
-    auto r = f(0,3,0,5);
-    if(r.second!=0){
-        std::cout << "loc=" << r.first-r.second << " len=" << r.second << "\n";
+    for (int i = 0; i < std::extent<decltype(T)>::value; i++)
+    {
+        auto r = f(i, std::extent<decltype(T)>::value, 0, std::extent<decltype(P)>::value);
+        if (r.second != 0)
+        {
+            std::cout << "loc=" << r.first - r.second << " len=" << r.second << "\n";
+        }
     }
     return 0;
 }
