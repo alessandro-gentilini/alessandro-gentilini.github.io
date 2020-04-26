eb0f5bb6415b0da65235731e0a65470219c476ab
Alessandro Gentilini <alessandro.gentilini@gmail.com>
2020-04-26 11:29:35 +0200

    Change rendering to non overlapping neato

--- COVID-19/vo.py
+++ COVID-19/vo.py
@@ -5,19 +5,17 @@ from graphviz import Digraph
 
 df = pd.read_excel('2020.04.17.20053157-2.xlsx',sheet_name='anonymised_dataset')
 
-# df = pd.read_csv("2020.04.17.20053157-2.csv")
-#print(df)
 
-# Each column represents a positive subjetc. Numbers in cells identify the type of contact occurred with the corresponding subjects in rows: 1=direct contact, as reported by the subject, 2=indirect contact (i.e. contact with a subject who had direct contact), 3=inferred contact (i.e. inferred from household)
-
-# for i in range(0,len(df.columns)):
-#     print(i,df.columns[i])
+# Each column represents a positive subjetc. 
+# Numbers in cells identify the type of contact occurred with the corresponding subjects in rows: 
+# 1=direct contact, as reported by the subject, 
+# 2=indirect contact (i.e. contact with a subject who had direct contact), 
+# 3=inferred contact (i.e. inferred from household)
 
 first_positive_column_id = 13
 last_positive_column_id = 103
 selector = df.iloc[:,first_positive_column_id:last_positive_column_id+1].notna()
 valids = df[selector.any(axis='columns')]
-
 
 #print(df['0db03881'].value_counts())
 
@@ -28,9 +26,8 @@ nodes = []
 for c in range(first_positive_column_id,last_positive_column_id+1):
     nodes.append({'id':str(df.columns[c]),'group':'positive'})
 
-
 links=[]
-dot = Digraph(comment='Vo Euganeo')
+dot = Digraph(comment='Vo Euganeo',engine='neato')
 for r in range(0,len(valids)):    
     for c in range(first_positive_column_id,last_positive_column_id+1):
         #if df.columns[c] != '0db03881' and df.columns[c] != '297bbbdc':
@@ -39,6 +36,7 @@ for r in range(0,len(valids)):
                 links.append({'source':str(df.columns[c]),'target':str(df.iloc[r,0])})
                 if not {'id':str(df.iloc[r,0])} in nodes:
                     nodes.append({'id':str(df.iloc[r,0]),'group':'subject'})
+dot.attr(overlap='false')                    
 dot.render('./gv/vo.gv')
 
 data = {"nodes":nodes,"links":links}
@@ -47,9 +45,8 @@ import json
 with open('vo_graph.json', 'w', encoding='utf-8') as f:
     json.dump(data, f, ensure_ascii=False, indent=4)
 
-
 for c in range(first_positive_column_id,last_positive_column_id+1):
-    dot = Digraph(comment=df.columns[c])
+    dot = Digraph(comment=df.columns[c],engine='neato')
     save_it = False
     contacts = 0
     for r in range(0,len(valids)):
@@ -59,8 +56,8 @@ for c in range(first_positive_column_id,last_positive_column_id+1):
             contacts = contacts+1
     if save_it:
         print(df.columns[c],contacts)
+        dot.attr(overlap='false')
         dot.render('./gv/'+df.columns[c]+'.gv')
-
 #print(valids)
 
 
