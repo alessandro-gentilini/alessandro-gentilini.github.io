0ba339927ec6b2bc87a2f991421ea09c7ade7f63
Alessandro Gentilini <alessandro.gentilini@gmail.com>
2020-05-03 14:33:40 +0200

    Redo

--- COVID-19/traffic_analysis.py
+++ COVID-19/traffic_analysis.py
@@ -90,12 +90,12 @@ df['Giorno'] = pd.to_datetime(df['Giorno'],format="%d/%m/%Y")
 df1 = df[df['Giorno']>='2020-02-01']
 df1 = df1.pivot_table(index='Giorno', columns=['Postazione','Corsia'],values='Transiti - Totale')
 df1 = 100*df1/df1.max()
-# fig, ax = plt.subplots(1)
-# df1.plot(legend=False,colormap='Blues',ax=ax)
-# ax.set_title('Flussi di traffico Emilia Romagna\n(Traffic in Emilia Romagna)\nData source: https://servizissiir.regione.emilia-romagna.it/FlussiMTS/')
-# ax.set_ylabel('Numero transiti giornalieri normalizzato\n(Normalized number of daily transits)\n100=max')
-# ax.set_xlabel('Data (date)')
-# ax.figure.savefig('Emilia_Romagna_all_stations.png',bbox_inches='tight')
+fig, ax = plt.subplots(1)
+df1.plot(legend=False,colormap='Blues',ax=ax)
+ax.set_title('Flussi di traffico Emilia Romagna\n(Traffic in Emilia Romagna)\nData source: https://servizissiir.regione.emilia-romagna.it/FlussiMTS/')
+ax.set_ylabel('Numero transiti giornalieri normalizzato\n(Normalized number of daily transits)\n100=max')
+ax.set_xlabel('Data (date)')
+ax.figure.savefig('Emilia_Romagna_all_stations.png',bbox_inches='tight')
 
 ys = []
 L = []
@@ -147,20 +147,20 @@ c2 = df['Giorno']>='2020-02-01'
 df2 = df[c1&c2]
 
 df3 = df2.pivot_table(index='Giorno', columns=['Postazione','Corsia'],values='Transiti - Totale')
-# fig, ax = plt.subplots(1)
-# df3.plot(legend=True,ax=ax)
-# ax.set_title('Strada: SS 9 tra Castel San Pietro e Imola\n(Traffic along road Via Emilia)\nData source: https://servizissiir.regione.emilia-romagna.it/FlussiMTS/')
-# ax.set_ylabel('Numero transiti giornalieri (number of daily transits)')
-# ax.set_xlabel('Data (date)')
-# ax.figure.savefig('station_255_Via_Emilia.png',bbox_inches='tight')
+fig, ax = plt.subplots(1)
+df3.plot(legend=True,ax=ax)
+ax.set_title('Strada: SS 9 tra Castel San Pietro e Imola\n(Traffic along road Via Emilia)\nData source: https://servizissiir.regione.emilia-romagna.it/FlussiMTS/')
+ax.set_ylabel('Numero transiti giornalieri (number of daily transits)')
+ax.set_xlabel('Data (date)')
+ax.figure.savefig('station_255_Via_Emilia.png',bbox_inches='tight')
 
-# df3 = 100*df3/df3.max()
-# fig, ax = plt.subplots(1)
-# df3.plot(legend=True,ax=ax)
-# ax.set_title('Strada: SS 9 tra Castel San Pietro e Imola\n(Traffic along road Via Emilia)\nData source: https://servizissiir.regione.emilia-romagna.it/FlussiMTS/')
-# ax.set_ylabel('Numero transiti giornalieri normalizzato\n(Normalized number of daily transits)\n100=max')
-# ax.set_xlabel('Data (date)')
-# ax.figure.savefig('station_255_Via_Emilia_normalized.png',bbox_inches='tight')
+df3 = 100*df3/df3.max()
+fig, ax = plt.subplots(1)
+df3.plot(legend=True,ax=ax)
+ax.set_title('Strada: SS 9 tra Castel San Pietro e Imola\n(Traffic along road Via Emilia)\nData source: https://servizissiir.regione.emilia-romagna.it/FlussiMTS/')
+ax.set_ylabel('Numero transiti giornalieri normalizzato\n(Normalized number of daily transits)\n100=max')
+ax.set_xlabel('Data (date)')
+ax.figure.savefig('station_255_Via_Emilia_normalized.png',bbox_inches='tight')
 
 
 
