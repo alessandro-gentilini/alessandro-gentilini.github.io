81a52764fdc6cf6cd1b980415f4338592619c76f
Alessandro Gentilini <alessandro.gentilini@gmail.com>
2020-05-10 17:41:23 +0200

    Add plot for different traffic comparisons

--- COVID-19/traffic_analysis.py
+++ COVID-19/traffic_analysis.py
@@ -5,6 +5,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import datetime as dt
 import numpy as np
+from matplotlib.lines import Line2D
 
 lanes = []
 stations = []
@@ -62,6 +63,58 @@ ax.set_xlabel('Data (date)')
 ax.figure.savefig('station_255_Via_Emilia_normalized.png',bbox_inches='tight')
 
 
+df4 = df[df['Giorno']>='2020-02-01']
+df4 = df4.pivot_table(index='Giorno', columns=['Postazione','Corsia'],values=['Transiti - Leggeri','Transiti - Pesanti'])
+df4 = 100*df4/df4.max()
+fig, ax = plt.subplots(1)
+colors={'Transiti - Leggeri':'blue','Transiti - Pesanti':'red'}
+for i in df4:
+    df4[i].plot(ax=ax,color=colors[i[0]], alpha=.1,lw=.2)
+
+lines = [Line2D([0], [0], color=c) for c in ('blue','red')]
+labels = ['Leggeri (light)', 'Pesanti (heavy)']
+ax.legend(lines, labels)    
+
+ax.set_title('Flussi di traffico Emilia Romagna\n(Traffic in Emilia Romagna)\nData source: https://servizissiir.regione.emilia-romagna.it/FlussiMTS/')
+ax.set_ylabel('Numero transiti giornalieri normalizzato\n(Normalized number of daily transits)\n100=max')
+ax.set_xlabel('Data (date)')
+ax.figure.savefig('Emilia_Romagna_all_stations_light_vs_heavy.png',bbox_inches='tight')
 
 
+
+df5 = df[df['Giorno']>='2020-02-01']
+df5 = df5.pivot_table(index='Giorno', columns=['Postazione','Corsia'],values=['Transiti - Diurno','Transiti - Notturno'])
+df5 = 100*df5/df5.max()
+fig, ax = plt.subplots(1)
+colors={'Transiti - Diurno':'yellow','Transiti - Notturno':'black'}
+for i in df5:
+    df5[i].plot(ax=ax,color=colors[i[0]], alpha=.1,lw=.2)
+
+lines = [Line2D([0], [0], color=c) for c in ('yellow','black')]
+labels = ['Diurno (Daytime)', 'Notturno (Night-time)']
+ax.legend(lines, labels)    
+
+ax.set_title('Flussi di traffico Emilia Romagna\n(Traffic in Emilia Romagna)\nData source: https://servizissiir.regione.emilia-romagna.it/FlussiMTS/')
+ax.set_ylabel('Numero transiti giornalieri normalizzato\n(Normalized number of daily transits)\n100=max')
+ax.set_xlabel('Data (date)')
+ax.figure.savefig('Emilia_Romagna_all_stations_daytime_vs_night.png',bbox_inches='tight')
+
+
+
+df6 = df[df['Giorno']>='2020-02-01']
+df6 = df6.pivot_table(index='Giorno', columns=['Postazione','Corsia'],values=['Transiti - Feriali','Transiti - Festivi'])
+df6 = 100*df6/df6.max()
+fig, ax = plt.subplots(1)
+colors={'Transiti - Feriali':'red','Transiti - Festivi':'green'}
+for i in df6:
+    df6[i].plot(ax=ax,color=colors[i[0]], alpha=.1,lw=.2)
+
+lines = [Line2D([0], [0], color=c) for c in ('red','green')]
+labels = ['Feriali (mon-fri)', 'Festivi (sat-sun)']
+ax.legend(lines, labels)    
+
+ax.set_title('Flussi di traffico Emilia Romagna\n(Traffic in Emilia Romagna)\nData source: https://servizissiir.regione.emilia-romagna.it/FlussiMTS/')
+ax.set_ylabel('Numero transiti giornalieri normalizzato\n(Normalized number of daily transits)\n100=max')
+ax.set_xlabel('Data (date)')
+ax.figure.savefig('Emilia_Romagna_all_stations_workday_vs_holiday.png',bbox_inches='tight')
 
