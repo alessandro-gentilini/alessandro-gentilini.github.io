f2bb06001d62c0696cb4741850b545e8eb702269
Alessandro Gentilini <alessandro.gentilini@gmail.com>
2020-09-13 15:10:28 +0200

    First results

--- pmp/pmp.cpp
+++ pmp/pmp.cpp
@@ -57,31 +57,34 @@ double measure_distance(const cv::Point2f& p1, const cv::Point2f& p2, const cv::
 int main(int, char**)
 {
     printf("OpenCV: %s", cv::getBuildInformation().c_str());
-    char cal_name[] = "IMG_3721.jpg";
+    char cal_name[] = "cal.jpg";
 
     cv::Size patternsize(4,3); //number of centers
     cv::Mat cal = cv::imread(cal_name);//, cv::IMREAD_GRAYSCALE);
-    std::vector<cv::Point2f> centers; //this will be filled by the detected centers
+    std::vector<cv::Point2f> img_px; //this will be filled by the detected centers
 
-    bool found = cv::findChessboardCorners( cal, patternsize, centers, cv::CALIB_CB_ADAPTIVE_THRESH );
+    bool found = cv::findChessboardCorners( cal, patternsize, img_px, cv::CALIB_CB_ADAPTIVE_THRESH );
 
     double square_side_mm = 30;
-    std::vector<cv::Point2f> world_mm;
+    double scale = 10;
+
+
+    std::vector< cv::Point2f > world_tenth_of_mm;
 
     std::cout << found << "\n";
     if(found){      
         // I need the first corner at top-left
-        if(centers.front().y > centers.back().y){
+        if(img_px.front().y > img_px.back().y){
             std::cout << "Reverse order\n";
-            std::reverse(centers.begin(),centers.end());
+            std::reverse(img_px.begin(),img_px.end());
         }
         for(size_t r=0;r<patternsize.height;r++){
             for(size_t c=0;c<patternsize.width;c++){
-                world_mm.push_back(cv::Point2f(c*square_side_mm,r*square_side_mm));
-                draw_cross(cal,centers[r*patternsize.width+c],20,CV_RGB(255, 0, 0));
+                world_tenth_of_mm.push_back(cv::Point2f(c*square_side_mm,r*square_side_mm));
+                draw_cross(cal,img_px[r*patternsize.width+c],5,CV_RGB(255, 0, 0));
                 std::ostringstream oss;
                 oss << "("<<r<<","<<c<<")";
-                cv::putText(cal, oss.str(), centers[r*patternsize.width+c], cv::FONT_HERSHEY_PLAIN, 3, CV_RGB(0, 255, 0), 3);
+                cv::putText(cal, oss.str(), img_px[r*patternsize.width+c], cv::FONT_HERSHEY_PLAIN, 1, CV_RGB(0, 255, 0), 1);
             }
         }
     }
@@ -91,40 +94,15 @@ int main(int, char**)
     cv::imwrite(std::string("Cal")+cal_name+".png", cal);
     
 
-    char img_name[] = "single-view-metrology.jpg";
+    char img_name[] = "quaderno.jpg";
     cv::Mat img = cv::imread(img_name);
     if(img.empty()){
         std::cerr << "Failure reading " << img_name << "\n";
         return 1;
     }
     std::cout << img.size() << "\n\n";
-    std::vector< cv::Point2f > world_tenth_of_mm;
-    std::vector< cv::Point2f > img_px;
 
-    // Here I manually picked the pixels coordinates of the corners of the A4 sheet.
-    cv::Point2f TL(711, 64);
-    cv::Point2f BL(317, 1429);
-    cv::Point2f TR(1970, 175);
-    cv::Point2f BR(1863, 1561);
 
-    // This is the standard size of the A4 sheet:
-    const int A4_w_mm = 210;
-    const int A4_h_mm = 297;
-    const int scale = 10;
-
-    // Here I create the correspondences between the world point and the
-    // image points.
-    img_px.push_back(TL);
-    world_tenth_of_mm.push_back(cv::Point2f(0.0, 0.0));
-
-    img_px.push_back(TR);
-    world_tenth_of_mm.push_back(cv::Point2f(A4_w_mm * scale, 0.0));
-
-    img_px.push_back(BL);
-    world_tenth_of_mm.push_back(cv::Point2f(0.0, A4_h_mm * scale));
-
-    img_px.push_back(BR);
-    world_tenth_of_mm.push_back(cv::Point2f(A4_w_mm * scale, A4_h_mm * scale));
 
     // Here I estimate the homography that brings the world to the image.
     cv::Mat H = cv::findHomography(world_tenth_of_mm, img_px);
@@ -138,38 +116,39 @@ int main(int, char**)
 
     // I can rectify the image.
     cv::Mat warped;
-    cv::warpPerspective(img, warped, G, cv::Size(2600, 2200 * 297 / 210));
+    //cv::warpPerspective(img, warped, G, cv::Size(square_side_mm*scale*(patternsize.width+1), square_side_mm*scale*(patternsize.height+1)));
+    cv::warpPerspective(img, warped, G, img.size());
 
 
-    {
-        // Here I manually picked the pixels coordinates of ticks '0' and '1' in the slide rule,
-        // in the world the distance between them is 10mm.
-        cv::Point2f tick_0(2017, 1159);
-        cv::Point2f tick_1(1949, 1143);
-        // I measure the distance and I write it on the image.
-        std::ostringstream oss;
-        oss << measure_distance(tick_0, tick_1, G) / scale;
-        cv::line(img, tick_0, tick_1, CV_RGB(0, 255, 0));
-        cv::putText(img, oss.str(), (tick_0 + tick_1) / 2, cv::FONT_HERSHEY_PLAIN, 3, CV_RGB(0, 255, 0), 3);
-    }
+    // {
+    //     // Here I manually picked the pixels coordinates of ticks '0' and '1' in the slide rule,
+    //     // in the world the distance between them is 10mm.
+    //     cv::Point2f tick_0(2017, 1159);
+    //     cv::Point2f tick_1(1949, 1143);
+    //     // I measure the distance and I write it on the image.
+    //     std::ostringstream oss;
+    //     oss << measure_distance(tick_0, tick_1, G) / scale;
+    //     cv::line(img, tick_0, tick_1, CV_RGB(0, 255, 0));
+    //     cv::putText(img, oss.str(), (tick_0 + tick_1) / 2, cv::FONT_HERSHEY_PLAIN, 3, CV_RGB(0, 255, 0), 3);
+    // }
 
-    {
-        // Here I manually picked the pixels coordinates of ticks '11' and '12' in the slide rule,
-        // in the world the distance between them is 10mm.
-        cv::Point2f tick_11(1277, 988);
-        cv::Point2f tick_12(1211, 973);
-        // I measure the distance and I write it on the image.
-        std::ostringstream oss;
-        oss << measure_distance(tick_11, tick_12, G) / scale;
-        cv::line(img, tick_11, tick_12, CV_RGB(0, 255, 0));
-        cv::putText(img, oss.str(), (tick_11 + tick_12) / 2, cv::FONT_HERSHEY_PLAIN, 3, CV_RGB(0, 255, 0), 3);
-    }
+    // {
+    //     // Here I manually picked the pixels coordinates of ticks '11' and '12' in the slide rule,
+    //     // in the world the distance between them is 10mm.
+    //     cv::Point2f tick_11(1277, 988);
+    //     cv::Point2f tick_12(1211, 973);
+    //     // I measure the distance and I write it on the image.
+    //     std::ostringstream oss;
+    //     oss << measure_distance(tick_11, tick_12, G) / scale;
+    //     cv::line(img, tick_11, tick_12, CV_RGB(0, 255, 0));
+    //     cv::putText(img, oss.str(), (tick_11 + tick_12) / 2, cv::FONT_HERSHEY_PLAIN, 3, CV_RGB(0, 255, 0), 3);
+    // }
 
-    // I draw the points used in the estimate of the homography.
-    draw_cross(img, TL, 40, CV_RGB(255, 0, 0));
-    draw_cross(img, TR, 40, CV_RGB(255, 0, 0));
-    draw_cross(img, BL, 40, CV_RGB(255, 0, 0));
-    draw_cross(img, BR, 40, CV_RGB(255, 0, 0));
+    // // I draw the points used in the estimate of the homography.
+    // draw_cross(img, TL, 40, CV_RGB(255, 0, 0));
+    // draw_cross(img, TR, 40, CV_RGB(255, 0, 0));
+    // draw_cross(img, BL, 40, CV_RGB(255, 0, 0));
+    // draw_cross(img, BR, 40, CV_RGB(255, 0, 0));
 
     cv::namedWindow( "Input image", cv::WINDOW_NORMAL );
     cv::imshow( "Input image", img );
