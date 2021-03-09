06b946fd0e3f5f227507fda0cea9ac5d4cb72487
Alessandro Gentilini <alessandro.gentilini@gmail.com>
2021-03-09 18:51:36 +0100

    Update with data for the 9th of March

--- COVID-19/analysis.py
+++ COVID-19/analysis.py
@@ -75,7 +75,7 @@ df3.plot(drawstyle='steps-mid',ax=ax3)
 ax3.axvspan('2021-02-01', '2021-02-21', alpha=0.5, color='yellow')
 ax3.axvspan('2021-02-21', '2021-02-25', alpha=0.5, color='orange')
 ax3.axvspan('2021-02-25', '2021-03-04', alpha=0.5, color='saddlebrown')
-ax3.axvspan('2021-03-04', '2021-03-08', alpha=0.5, color='red')
+ax3.axvspan('2021-03-04', '2021-03-09', alpha=0.5, color='red')
 ax3.set_xlabel('')
 ax3.set_ylabel('persone (person)')
 # translation according to https://github.com/pomber/covid19
