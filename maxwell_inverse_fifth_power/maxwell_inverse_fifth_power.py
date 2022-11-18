9118049f7904eac87111c004da438b4c65c39852
Alessandro Gentilini <alessandro.gentilini@gmail.com>
2022-11-18 11:32:55 +0100

    Toward brute force

--- maxwell_inverse_fifth_power/maxwell_inverse_fifth_power.py
+++ maxwell_inverse_fifth_power/maxwell_inverse_fifth_power.py
@@ -4,9 +4,13 @@ from scipy.integrate import odeint
 # The fabulous numpy library:
 import numpy as np
 import matplotlib.pyplot as plt
+import math
+
+def p2ld(x0,y0,x1,y1,x2,y2):
+    return abs((x2-x1)*(y1-y0)-(x1-x0)*(y2-y1))/math.sqrt((x2-x1)**2+(y2-y1)**2)
 
 # Parameters
-G = 1
+k = 1
 x0 = 0
 y0 = 3
 vx0 = 0
@@ -20,9 +24,9 @@ vy0 = -1
 # In general, F can depend upon time t as well. Although our F is independent of t, we still need to
 # indicate that it is a possible variable.
 
-def F(s,t): return [s[2],s[3],
-    G*s[0]/(s[0]**2 + s[1]**2)**(6/2),
-    G*s[1]/(s[0]**2 + s[1]**2)**(6/2),
+def Fun(s,t): return [s[2],s[3],
+    k*s[0]/(s[0]**2 + s[1]**2)**(6/2),
+    k*s[1]/(s[0]**2 + s[1]**2)**(6/2),
 ]
 
 # We'll solve the system for the following values of t, namely
@@ -35,7 +39,7 @@ Delta_x = .1
 
 for i in steps:
     # Solve it!
-    solution = odeint(F,[x0+i*Delta_x,y0,vx0,vy0],t)
+    solution = odeint(Fun,[x0+i*Delta_x,y0,vx0,vy0],t)
     x = solution[:,0]
     y = solution[:,1]
     ax.plot(x,y, '-')
@@ -43,31 +47,74 @@ for i in steps:
 
 ax.plot([0],[0], '*')
 
-G = 1000
+k = 1000
 t = np.linspace(0,5,100)
+# v1
+A = (1.1,9.4)
+B = (1.1,4.9)
+
+# d1
+C = (2.7,4.9)
+D = (6.8,9.4)
+
+# v2
+E = (1.8,9.4)
+F = (1.8,4.9)
+
+# d2
+G = (5.3,4.9)
+H = (7,6)
 
 fig, ax = plt.subplots(1)
-solution = odeint(F,[1.1,4.1,0,-1.5],t)
+solution = odeint(Fun,[1.1,4.1,0,-1.5],t)
 x = solution[:,0]
 y = solution[:,1]
-ax.plot(x,y, '-')
+#ax.plot(x,y, '-')
 ax.set_aspect('equal', 'box')
 
-solution = odeint(F,[1.8,4.1,0,-1.5],t)
+accum = 0
+for r in solution:
+    v1 = p2ld(r[0],r[1],A[0],A[1],B[0],B[1])
+    d1 = p2ld(r[0],r[1],C[0],C[1],D[0],D[1])
+    if v1 < d1:
+        accum = accum + v1
+        ax.plot(r[0],r[1], 'd',color='C0')
+    else:
+        accum = accum + d1
+        ax.plot(r[0],r[1], 'x',color='C0')
+
+solution = odeint(Fun,[1.8,4.1,0,-1.5],t)
 x = solution[:,0]
 y = solution[:,1]
-ax.plot(x,y, '-')
+#ax.plot(x,y, '-')
 ax.set_aspect('equal', 'box')
+
+accum = 0
+for r in solution:
+    v2 = p2ld(r[0],r[1],E[0],E[1],F[0],F[1])
+    d2 = p2ld(r[0],r[1],G[0],G[1],H[0],H[1])
+    if v2 < d2:
+        accum = accum + v2
+        ax.plot(r[0],r[1], 'd',color='C1')
+    else:
+        accum = accum + d2
+        ax.plot(r[0],r[1], 'x',color='C1')
+
+
 
 #ax.axline((1.1,9.4),(1.2,4.9),ls='--',color='C0')
-ax.axline((1.1,9.4),(1.1,4.9),ls='--',color='C0')
-ax.axline((2.7,4.9),(6.8,9.4),ls='--',color='C0')
+ax.axline(A,B,ls='--',color='C0')
+ax.axline(C,D,ls='--',color='C0')
 
 #ax.axline((1.8,9.4),(1.9,4.9),ls='--',color='C1')
-ax.axline((1.8,9.4),(1.8,4.9),ls='--',color='C1')
-ax.axline((5.3,4.9),(7,6),ls='--',color='C1')
+ax.axline(E,F,ls='--',color='C1')
+ax.axline(G,H,ls='--',color='C1')
 
 
 ax.plot([0],[0], '*')
+
+
+
+print(accum)
 
 plt.show()
