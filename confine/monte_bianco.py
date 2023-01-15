482c40eed7b22e1325ddb05c9c9c960f051ba9cd
Alessandro Gentilini <alessandro.gentilini@gmail.com>
2023-01-15 14:30:27 +0100

    copr the tif

--- confine/monte_bianco.py
+++ confine/monte_bianco.py
@@ -5,11 +5,40 @@ import matplotlib.pyplot as plt
 from shapely.geometry import Point
 import pandas as pd
 from rasterio.plot import show
+import rasterio.mask
 
-dem = rasterio.open('Pleiades_Mont-Blanc_2017-10-25_DEM_5m.tif')
+monte_bianco = (334162,5077700)
+crop_side = 1000
+
+# https://github.com/rasterio/rasterio/issues/1343#issuecomment-389971813
+geoms = [{'type': 'Polygon', 'coordinates': [
+        [
+            (monte_bianco[0]-crop_side/2,monte_bianco[1]-crop_side/2), 
+            (monte_bianco[0]+crop_side/2,monte_bianco[1]-crop_side/2),
+            (monte_bianco[0]+crop_side/2,monte_bianco[1]+crop_side/2),
+            (monte_bianco[0]-crop_side/2,monte_bianco[1]+crop_side/2),
+            (monte_bianco[0]-crop_side/2,monte_bianco[1]-crop_side/2)
+        ]]}]
+
+# https://rasterio.readthedocs.io/en/latest/topics/masking-by-shapefile.html
+with rasterio.open("Pleiades_Mont-Blanc_2017-10-25_DEM_5m.tif") as src:
+    out_image, out_transform = rasterio.mask.mask(src, geoms, crop=True)
+    out_meta = src.meta
+
+out_meta.update({"driver": "GTiff",
+                 "height": out_image.shape[1],
+                 "width": out_image.shape[2],
+                 "transform": out_transform})
+
+with rasterio.open("pleiades.masked.tif", "w", **out_meta) as dest:
+    dest.write(out_image)
+
+
+dem = rasterio.open('pleiades.masked.tif')
+
 
 # https://www.istat.it/storage/cartografia/confini_amministrativi/non_generalizzati/Limiti01012022.zip
-boundary_it=geopandas.read_file('Limiti01012022/Com01012022/Com01012022_WGS84.shp')# todo conciliare i crs!!!
+boundary_it=geopandas.read_file('Limiti01012022/Com01012022/Com01012022_WGS84.shp')
 boundary_it = boundary_it.loc[boundary_it['COMUNE']=="Courmayeur"]
 boundary_it = boundary_it.to_crs(epsg=dem.crs.to_epsg())
 
@@ -26,8 +55,8 @@ fig, ax = plt.subplots()
 extent=[dem.bounds[0], dem.bounds[2], dem.bounds[1], dem.bounds[3]]
 ax = rasterio.plot.show(dem, extent=extent, ax=ax)#, cmap='terrain')
 
-ax.axhline(y=5077700, color='gray', linestyle='-')
-ax.axvline(x=334162, color='gray', linestyle='-')
+ax.axhline(y=monte_bianco[1], color='gray', linestyle='-')
+ax.axvline(x=monte_bianco[0], color='gray', linestyle='-')
 
 boundary.plot(ax=ax,facecolor="none", edgecolor=["red","black","blue"])
 
