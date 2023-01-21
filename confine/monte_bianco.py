c4f20f2b260fa22833688b97f95366d10ba43315
Alessandro Gentilini <alessandro.gentilini@gmail.com>
2023-01-21 15:41:54 +0100

    First result for countour labels

--- confine/monte_bianco.py
+++ confine/monte_bianco.py
@@ -55,10 +55,12 @@ boundary_fr = boundary_fr.to_crs(epsg=dem.crs.to_epsg())
 boundary = pd.concat([boundary_it['geometry'],boundary_fr['geometry']])
 
 fig, ax = plt.subplots()
+fig2, ax2 = plt.subplots()
 
 # transform rasterio plot to real world coords
 extent=[dem.bounds[0], dem.bounds[2], dem.bounds[1], dem.bounds[3]]
 ax = rasterio.plot.show(dem, extent=extent, ax=ax, cmap='plasma')
+ax2 = rasterio.plot.show(dem, extent=extent, ax=ax2, cmap='plasma')
 
 #ax.axhline(y=monte_bianco[1], color='gray', linestyle='-',linewidth=.1)
 #ax.axvline(x=monte_bianco[0], color='gray', linestyle='-',linewidth=.1)
@@ -70,6 +72,9 @@ ylim = ([monte_bianco[1]-crop_side/2, monte_bianco[1]+crop_side/2])
 ax.set_xlim(xlim)
 ax.set_ylim(ylim)
 
+ax2.set_xlim(xlim)
+ax2.set_ylim(ylim)
+
 raw_dem=dem.read()[0]
 if np.any(raw_dem == -9999):
     raw_dem[raw_dem == -9999] = np.nan
@@ -79,63 +84,62 @@ qcs = plt.contour(raw_dem,levels = list(range(4000, 5000, 25)))
 meter_per_px_x = abs(dem.transform[0])
 meter_per_px_y = abs(dem.transform[4])
 
-idx = 0
-lw = .1
-for collection in qcs.collections:
+# https://codereview.stackexchange.com/a/28210
+def shortest_distance(node, nodes):
+    dist_2 = np.sum((nodes - node)**2, axis=1)
+    return {'sqdist':np.min(dist_2),'p':nodes[np.argmin(dist_2)]}
+
+main_contour = False
+for idx,collection in enumerate(qcs.collections):
     if int(qcs.levels[idx]) % 100 == 0:
-        lw=.3
+        main_contour=True
     else:
-        lw=.1
+        main_contour=False
+
     for path in collection.get_paths():
         for polygon in path.to_polygons(): 
             polygon[:,0]=meter_per_px_x*(polygon[:,0]-raw_dem.shape[0]/2)
             polygon[:,1]=meter_per_px_y*(polygon[:,1]-raw_dem.shape[1]/2)
             polygon[:,0]=polygon[:,0]+monte_bianco[0]
             polygon[:,1]=polygon[:,1]+monte_bianco[1]
-            ax.add_patch(Polygon(polygon,fill=None,closed=False,linewidth=lw))
-    idx = idx+1
+            if main_contour:
+                t = ax2.text(monte_bianco[0],monte_bianco[1],str(qcs.levels[idx]),{'ha': 'center', 'va': 'center'},rotation=0)
+                renderer = fig.canvas.get_renderer()
+                bbox = plt.gca().transData.inverted().transform_bbox(t.get_window_extent(renderer))      
+                min_distance = np.inf
+                min_p = polygon[0]
+                min_angle = 0
+                min_R = polygon[0]
+                min_L = polygon[0]
+                for i,p in enumerate(polygon):
+                    L=np.array([p[0]-bbox.width/2,p[1]])
+                    R=np.array([p[0]+bbox.width/2,p[1]])
+                    for d in range(0,360):
+                        L_0 = L-p
+                        R_0 = R-p
+                        theta = math.radians(d)
+                        M = np.array([[math.cos(theta),-math.sin(theta)],[math.sin(theta),math.cos(theta)]])
+                        L_rot = np.dot(M,L_0)+p
+                        R_rot = np.dot(M,R_0)+p
+                        pp = np.vstack([polygon[0:i],polygon[i+1:]])
+                        d_L = shortest_distance(L_rot,pp)
+                        d_R = shortest_distance(R_rot,pp)
+                        if d_L['sqdist']+d_R['sqdist']<min_distance:
+                            min_distance = d_L['sqdist']+d_R['sqdist']
+                            min_p = p
+                            min_angle = d
+                            min_R = d_R['p']
+                            min_L = d_L['p']
+                    #ax.plot(p[0],p[1],'.',color='black')
 
-t = ax.text(monte_bianco[0],monte_bianco[1],'Monte Bianco',{'ha': 'center', 'va': 'center'},rotation=0)
-renderer = fig.canvas.get_renderer()
-bbox = plt.gca().transData.inverted().transform_bbox(t.get_window_extent(renderer))
+                #ax.plot(min_R[0],min_R[1],'o',color='red')
+                #ax.plot(min_L[0],min_L[1],'x',color='blue')
+                #ax.plot(min_p[0],min_p[1],'+',color='green')
 
-# https://codereview.stackexchange.com/a/28210
-def shortest_distance(node, nodes):
-    dist_2 = np.sum((nodes - node)**2, axis=1)
-    return {'sqdist':np.min(dist_2),'p':nodes[np.argmin(dist_2)]}
-
-min_distance = np.inf
-min_p = polygon[0]
-min_angle = 0
-min_R = polygon[0]
-min_L = polygon[0]
-for i,p in enumerate(polygon):
-    L=np.array([p[0]-bbox.width/2,p[1]])
-    R=np.array([p[0]+bbox.width/2,p[1]])
-    for d in range(0,360):
-        L_0 = L-p
-        R_0 = R-p
-        theta = math.radians(d)
-        M = np.array([[math.cos(theta),-math.sin(theta)],[math.sin(theta),math.cos(theta)]])
-        L_rot = np.dot(M,L_0)+p
-        R_rot = np.dot(M,R_0)+p
-        pp = np.vstack([polygon[0:i],polygon[i+1:]])
-        d_L = shortest_distance(L_rot,pp)
-        d_R = shortest_distance(R_rot,pp)
-        if d_L['sqdist']+d_R['sqdist']<min_distance:
-            min_distance = d_L['sqdist']+d_R['sqdist']
-            min_p = p
-            min_angle = d
-            min_R = d_R['p']
-            min_L = d_L['p']
-    ax.plot(p[0],p[1],'.',color='black')
-
-ax.plot(min_R[0],min_R[1],'o',color='red')
-ax.plot(min_L[0],min_L[1],'x',color='blue')
-ax.plot(min_p[0],min_p[1],'+',color='green')
-
-ax.text(min_p[0],min_p[1],'Monte Bianco',{'ha': 'center', 'va': 'center'},rotation=min_angle)
-
+                ax.text(min_p[0],min_p[1],str(qcs.levels[idx]),{'ha': 'center', 'va': 'center'},rotation=min_angle)          
+                ax.add_patch(Polygon(polygon,fill=None,closed=False,linewidth=.3))
+            else:
+                ax.add_patch(Polygon(polygon,fill=None,closed=False,linewidth=.1))
 
 boundary.plot(ax=ax,facecolor="none", edgecolor=["blue","red","red"],linestyle='-.')
 
