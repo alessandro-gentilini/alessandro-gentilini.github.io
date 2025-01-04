befc49c0b32aa5947019438b40f882d63cb547d7
Alessandro <alessandro.gentilini@gmail.com>
2025-01-04 16:42:05 +0100

    controlla data

--- geo/le_mie_note.py
+++ geo/le_mie_note.py
@@ -2,6 +2,49 @@ import bibtexparser
 from bibtexparser.bparser import BibTexParser
 from bibtexparser.customization import convert_to_unicode
 
+import locale
+from datetime import datetime
+
+def has_month(str):
+    months = ['gennaio', 
+              'febbraio', 
+              'marzo', 
+              'aprile', 
+              'maggio', 
+              'giugno', 
+              'luglio', 
+              'agosto', 
+              'settembre', 
+              'ottobre', 
+              'novembre', 
+              'dicembre']
+    longest_string_with_a_date = '30 settembre 2025'
+    if len(str)>len(longest_string_with_a_date):
+        return False
+    for m in months:
+        if m in str.lower():
+            return True
+    return False
+
+def get_last_line(input_string):
+    # Split the string by newline characters
+    lines = input_string.split('\n')
+    # Return the last line
+    return lines[-1] if lines else ''
+
+def parse_italian_date(date_str):
+    # Set the locale to Italian
+    locale.setlocale(locale.LC_TIME, 'it_IT.UTF-8')
+
+    # Define the date format
+    date_format = '%d %B %Y'
+
+    # Parse the date string
+    parsed_date = datetime.strptime(date_str, date_format)
+
+    return parsed_date
+    parsed_date = parse_italian_date(italian_date_str)
+
 def create_latex_from_bib(bib_file_path, output_tex_path):
     with open(bib_file_path, 'r', encoding='utf-8') as bib_file:
         parser = BibTexParser()
@@ -23,6 +66,12 @@ def create_latex_from_bib(bib_file_path, output_tex_path):
                 continue
             citation_key = entry.get('ID', None)
             if annote and citation_key:
+                last_line = get_last_line(annote)
+                if has_month(last_line):
+                    try:
+                        print(last_line+'\t'+str(parse_italian_date(last_line)))
+                    except:
+                        print(last_line)
                 tex_file.write(f'{annote}\n')
                 tex_file.write(f'\\cite{{{citation_key}}}\n\n')
                 tex_file.write(r'\par\noindent\rule{\textwidth}{0.4pt}')
