266253edd81fd747866884a994ef0f33244fa58b
Alessandro <alessandro.gentilini@gmail.com>
2025-03-11 07:00:26 +0100

    aggiorno

--- geo/riconoscimento_rocce/build.sh
+++ geo/riconoscimento_rocce/build.sh
@@ -1,1 +1,3 @@
 pandoc appunti.md -H figure_placement.tex -o appunti.pdf
+pandoc 2025-02-26_laboratorio_rocce.md -H figure_placement.tex -o 2025-02-26_laboratorio_rocce.pdf
+pandoc 2025-03-05_laboratorio_rocce.md -H figure_placement.tex -o 2025-03-05_laboratorio_rocce.pdf
