1b58e9afa1502e1436e9aab7c47c279fa36af4b0
Alessandro <alessandro.gentilini@gmail.com>
2025-10-21 22:29:44 +0200

    così però non funziona

--- geo/geomorfologia/fattore_di_sicurezza.py
+++ geo/geomorfologia/fattore_di_sicurezza.py
@@ -16,30 +16,36 @@ def asciutto(rho_b,g,h,theta,c,phi):
     return FS(rho_b,g,h,theta,c,phi,0)
 
 g = 9.8
-rho_b = 1650
+rho = 2650
+fill_factor = .6
+rho_w = 1000
 h = 1
 phi=15
-c = 0#rho_b*g*h
 
+rho_b = fill_factor*rho
+c = 0#rho_b*g*h
 thetas = [x * 0.5 for x in range(2, 141)]
 thetas = np.arange(1, 70, 0.5)
 FS_asciutto = []
 for theta in thetas:
     FS_asciutto.append(asciutto(rho_b,g,h,theta,c,phi))
 
-g = 9.8
-rho_b = 1990
-h = 1
-phi=15
-c = 0#rho_b*g*h
-rho_w = 1000
 
+rho_b = fill_factor*rho+(1-fill_factor)*rho_w
+c = 0#rho_b*g*h
 FS_bagnato = []
 for theta in thetas:
     FS_bagnato.append(bagnato(rho_b,g,h,theta,c,phi,rho_w))
 
+rho_b = fill_factor*rho+((1-fill_factor)*rho_w)/2
+c = 0#rho_b*g*h
+FS_semi_asciutto = []
+for theta in thetas:
+    FS_semi_asciutto.append(bagnato(rho_b,g,h,theta,c,phi,rho_w))
+
 plt.plot(thetas, FS_asciutto, color='brown')
-plt.plot(thetas, FS_bagnato, color='blue')
+plt.plot(thetas, FS_semi_asciutto, color='tab:green')
+plt.plot(thetas, FS_bagnato, color='tab:blue')
 plt.xlabel(r'$\vartheta$/°')
 plt.ylabel(r'FS')
 plt.axhline(1, color='gray')
